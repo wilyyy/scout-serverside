ab4e327b60f87df36c4d756dde203f20875f4054
wilyyy <walvarez1@my.bcit.ca>
2022-03-22 03:35:39 -0700

    need to have a user token to add to ur list and get from ur list now

--- Routes/yourList.js
+++ Routes/yourList.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { GetAllFromYourList, AddToYourList } = require("../Controller/yourList");
+const AuthorizeUser = require("../Auth/auth");
 
-router.get("/yourList/getAllAnimes", GetAllFromYourList);
+router.get("/yourList/getAllAnimes", AuthorizeUser, GetAllFromYourList);
 
-router.post("/yourList/addAnime", AddToYourList);
+router.post("/yourList/addAnime", AuthorizeUser, AddToYourList);
 
 module.exports = router;
